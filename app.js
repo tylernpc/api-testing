fb53a77900f6277855cfccb04795f37226d272af
tylernpc <103677076+tylernpc@users.noreply.github.com>
2023-11-16 20:06:57 -0600

    Reddit Market Sentiment analysis success

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-const apiEndpoint = 'https://partners.api.skyscanner.net/apiservices/v3/flights/live/search/create';
+const apiEndpoint = 'https://tradestie.com/api/v1/apps/reddit';
 
-const requestData = {
-    "market": "US",
-    "currency": "USD",
-    "locale": "en-US",
-    "originPlace": "SFO-sky",
-    "destinationPlace": "LON-sky",
-    "outboundDate": "2023-12-01",
-    "adults": 1
-};
+async function fetchRedditSentiment(date = '') {
+    try {
+        const url = date ? `$(apiEndpoint)?date=${date}` : apiEndpoint;
 
-fetch(apiEndpoint, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(requestData)
-}).then(response => {
-    // check if the request was successful (status code 2xx)
-    if (response.ok) {
-        // parse and log the json response
-        return response.json();
-    } else {
-        // log an error message if the request was unsuccessful
-        console.error(`Error: ${response.status}`);
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data. Status: ${response.status}`)
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
     }
-}).then(data => {
-    // do something with the response data
-    console.log('Response:', data);
-}).catch(error => {
-    // log any errors that occurred during the fetch
-    console.error('Fetch error:', error);
-});
+}
+
+fetchRedditSentiment();
+
+fetchRedditSentiment('2022-04-03');
